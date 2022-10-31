eec32385f3dc0e86eeac6601b5f01f263cf17db8
Cauã dantas <100371641+Cauadsouza@users.noreply.github.com>
2022-10-31 09:27:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+/*mundos*/
+var unicadiv = window.document.getElementById('div_ex')
+
+var unicadiv2 = window.document.getElementById('div_ex2')
+
+var unicadiv3 = window.document.getElementById('div_ex3')
+
+var unicadiv4 = window.document.getElementById('div_ex4')
+
+var unicadiv5 = window.document.getElementById('div_ex5')
+
+var unicadiv9 = window.document.getElementById('div_ex9')
+
+var unicadiv10 = window.document.getElementById('div_ex10')
+
+var unicadiv11 = window.document.getElementById('div_ex11')
+
+var unicadiv12 = window.document.getElementById('div_ex12')
+
+var unicadiv13 = window.document.getElementById('div_ex13')
+
+var unicadiv14 = window.document.getElementById('div_ex14')
+
+function exibir(){
+    unicadiv.style.display = "flex"
+}
+
+function exibir2(){
+    unicadiv2.style.display = "flex"
+}
+
+function exibir3(){
+    unicadiv3.style.display = "flex"
+}
+
+function exibir4(){
+    unicadiv4.style.display = "flex"
+}
+
+function exibir5(){
+    unicadiv5.style.display = "flex"
+}
+
+function exibir9(){
+    unicadiv9.style.display = "flex"
+}
+
+function exibir10(){
+    unicadiv10.style.display = "flex"
+}
+
+function exibir11(){
+    unicadiv11.style.display = "flex"
+}
+
+function exibir12(){
+    unicadiv12.style.display = "flex"
+}
+
+function exibir13(){
+    unicadiv13.style.display = "flex"
+}
+
+function exibir14(){
+    unicadiv14.style.display = "flex"
+}
+
+function fechar(){
+    unicadiv.style.display = "none"
+    unicadiv2.style.display = "none"
+    unicadiv3.style.display = "none"
+    unicadiv4.style.display = "none"
+    unicadiv5.style.display = "none"
+    unicadiv9.style.display = "none"
+    unicadiv10.style.display = "none"
+    unicadiv11.style.display = "none"
+    unicadiv12.style.display = "none"
+    unicadiv13.style.display = "none"
+    unicadiv14.style.display = "none"
+}
