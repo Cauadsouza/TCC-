b47da77791b3eb6d0fa30abf5d1c13da682f592a
Cauã dantas <100371641+Cauadsouza@users.noreply.github.com>
2022-11-22 15:39:51 -0300

    update

--- TCC/APIs/index.js
+++ TCC/APIs/index.js
@@ -1,24 +1,45 @@
 const express = require('express');
 const server = express ();
 const Alimentos = require('./src/data/Alimentos.json')
+const AlimentosIF = require('./src/data/AlimnetosIF.json')
 
-server.get('/Alimentos', (req,res) =>{
-    res.json(Alimentos)
-});
-
-server.listen(300, () => {
+server.listen(3000, () => {
     console.log("Funcionando...")
 });
 
-server.get('/Alimentos/:id', (req, res) => {
+/*API Alimentos*/
+
+server.get('/alimentos', (req,res) =>{
+    res.json(Alimentos)
+});
+
+server.get('/alimentos/:id', (req, res) => {
     let id = req.params.id;
     let prod = Alimentos.filter(p => p.id == id)[0];
     res.json(prod);    
 })
 
 server.get('/alimentos/nome/:nm', (req, res) => {
-    let nome = req.params.nome;
+    let nome = req.params.nm;
     let nm = Alimentos.filter(n => n.Nome == nome);
+    res.json(nm);    
+})
+
+/*API AlimentosIF*/
+
+server.get('/alimentosIF', (req,res) =>{
+    res.json(AlimentosIF)
+});
+
+server.get('/alimentosIF/:id', (req, res) => {
+    let id = req.params.id;
+    let prod = AlimentosIF.filter(p => p.id == id)[0];
+    res.json(prod);    
+})
+
+server.get('/alimentosIF/nome/:nm', (req, res) => {
+    let nome = req.params.nm;
+    let nm = AlimentosIF.filter(n => n.Nome == nome);
     res.json(nm);    
 })
 
--- TCC/src/app/app.module.ts
+++ TCC/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/plnpm atform-browser';
 import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
