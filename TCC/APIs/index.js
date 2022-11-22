9b512b6894077c95413371b17efaaf6db2b81d91
Cauã dantas <100371641+Cauadsouza@users.noreply.github.com>
2022-11-22 01:13:27 -0300

    çp

---
+++ TCC/APIs/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const server = express ();
+const Alimentos = require('./src/data/Alimentos.json')
+
+server.get('/Alimentos', (req,res) =>{
+    res.json(Alimentos)
+});
+
+server.listen(300, () => {
+    console.log("Funcionando...")
+});
+
+server.get('/Alimentos/:id', (req, res) => {
+    let id = req.params.id;
+    let prod = Alimentos.filter(p => p.id == id)[0];
+    res.json(prod);    
+})
+
+server.get('/Alimentos/:Nome', (req, res) => {
+    let Nome = req.params.Nome;
+    let nm = Alimentos.filter(n => n.Nome == Nome);
+    res.json(nm);    
+})
+
