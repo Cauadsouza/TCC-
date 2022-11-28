f64bcdd0af866e5dc1037b1f46bf9bd35f61531e
Cauã dantas <100371641+Cauadsouza@users.noreply.github.com>
2022-11-28 20:32:46 -0300

    update

---
+++ TCC/src/app/Serviços/Alimentos.ts
@@ -0,0 +1,11 @@
+export interface DadosNutricionais{
+    id:number;
+    Nome:string;
+    Peso:number;
+    Calorias:number;
+    Gordura:number;
+    Sódio:number;
+    Carboidratos:number;
+    Proteína:number;
+    Fibra:number
+}
--- TCC/src/app/Serviços/lista-alimentos.ts
+++ TCC/src/app/Serviços/lista-alimentos.ts
@@ -1,10 +1,10 @@
-export interface LA {
-    id:number;
-    Nome:string;
-    Calorias:number;
-    Gordura:number;
-    Sodio:number;
-    Carboidratos:number;
-    Proteina:number;
-    Fibra:number
+export class LA {
+    id:number = 0;
+    Nome:string = "";
+    Calorias:number = 0;
+    Gordura:number = 0;
+    Sodio:number = 0;
+    Carboidratos:number = 0;
+    Proteina:number = 0;
+    Fibra:number = 0
 }
